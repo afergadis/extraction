94a370a1a67501c24a8014149d7e0a8066754d48
Aris Fergadis <fergadis.aris@gmail.com>
2013-11-06 12:08:35 +0200

    Preprocessing is moved in a seperate class. Sphinx configuration files are added.

--- extract.py
+++ extract.py
@@ -4,12 +4,27 @@ __version__ = 0.1
 import codecs
 import logging
 import sys
-from operator import itemgetter
 
-import nltk
+from operator import itemgetter
+from preprocess import Preprocess
 from gensim import corpora, models
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+def sentences_length(sentences):
+    """
+    Returns the shortest and longest acceptable sentences sentences length
+
+    The shortest is 50% less of the average sentence length and the longest 50% more
+    """
+    summary = 0
+    for s in sentences:
+        summary += len(s)
+    avg = 1.0 * summary / len(sentences)
+    long_len = int(avg * 1.5)
+    return long_len
+
 
 if len(sys.argv) != 3:
     file_to_open = raw_input("File to open: ")
@@ -23,40 +38,37 @@ f = codecs.open(file_to_open, encoding='utf-8')
 article = f.read()
 f.close()
 
-# Sentence segmentation and tokenization
-document_orig = nltk.sent_tokenize(article)
-document = [nltk.word_tokenize(line) for line in document_orig]
-
-# Remove stop words and punctuation
-stopwords = open('stopwords/english').read().splitlines()
-punctuation = ['.', ',', ';', ':', '!', '?', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', "'"]
-stoplist = stopwords + punctuation
-sentences = [[word.lower() for word in sentence
-              if word.lower() not in stoplist]
-             for sentence in document]
-
-# Remove words that appears only once
-all_tokens = sum(sentences, [])
-tokens_once = set(word for word in set(all_tokens) if all_tokens.count(word) == 1)
-sentences = [[word for word in text if word not in tokens_once]
-             for text in sentences]
+# Preprocess article
+ppd = Preprocess(article)
+sentences = ppd.sentences
+dictionary_text = ppd.dictionary_text
 
 # Assign a unique integer id to all words appearing in the document
-dictionary = corpora.Dictionary(sentences)
+dictionary = corpora.Dictionary(dictionary_text)
 
 # The function doc2bow() simply counts the number of occurrences of each
 # distinct word, converts the word to its integer word id and returns the
 # result as a sparse vector.
-corpus = [dictionary.doc2bow(sent) for sent in sentences]
+corpus = [dictionary.doc2bow(sent) for sent in dictionary_text]
 
 # Initialize the tfidf model
 tfidf = models.TfidfModel(corpus)
 corpus_tfidf = tfidf[corpus]
 
+# Remove paragraphs that are too short_len or too long_len
+long_len = sentences_length(sentences)
+sents_2_remove = []
+for sent_id, sent in enumerate(sentences):
+    sent_len = len(sent)
+    if sent_len > long_len:
+        sents_2_remove.append(sent_id)
+
 # Calculate the sentences' score
 sentences_score = []
 for sent_id, doc in enumerate(corpus_tfidf):
     sent_score = 0
+    if sent_id in sents_2_remove:
+        continue
     for term_id, term_score in doc:
         sent_score += term_score
     sentences_score.append((sent_id, sent_score))
@@ -66,13 +78,13 @@ extract_len = int(round(len(sentences) * (int(summary_len)) / 100.0))
 
 # Sort sentences by score in descending order
 sorted_sents_by_score = sorted(sentences_score, key=itemgetter(1), reverse=True)
-# Sort hi score sentences by sentence number as the appear in document
+# Sort hi score sentences by sentence number as the appear in document_words
 hi_score_sents = sorted(sorted_sents_by_score[:extract_len], key=itemgetter(0))
 
 # Some statistics
 print "Document sentences: {0}".format(len(sentences))
 print "Extraction sentences: {0}\n".format(int(round(len(sentences) * (int(summary_len)) / 100.0)))
-#print document_orig[0]  # print title
+#print document_sents[0]  # print title
 for sent in hi_score_sents:
     sent_id, _ = sent
-    print document_orig[sent_id]
+    print sentences[sent_id]
---
+++ index.rst
@@ -0,0 +1,22 @@
+.. Auto Extract documentation master file, created by
+   sphinx-quickstart on Sun Oct 27 16:24:01 2013.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Auto Extract's documentation!
+========================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
